0ca478a5df74df8173573dfd8572074d7c8e871d
ccpoe9 <79650652+ccpoe9@users.noreply.github.com>
2022-01-26 13:59:27 -0500

    Add files via upload

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.theperfectfit.theperfectfit {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.theperfectfit.theperfectfit to javafx.fxml;
+    exports com.theperfectfit.theperfectfit;
+}
